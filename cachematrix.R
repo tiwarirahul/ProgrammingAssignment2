52613de21ed5c0a24d3bdc3b60f3d4111d2da433
tiwarirahul <rahultiwari.iet@gmail.com>
2015-07-23 01:30:48 +0530

    Update cachematrix.R
    Added code for ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cacheSolve function accepts a special matrix object return from makeCacheMatrix function, and check whether inverse of the matrix
+## provided to makeCacheMatrix function is already calculated. If yes, then return cached inverse matrix, else compute inverse of the ## matrix
 
-## Write a short comment describing this function
+## Accepts a Matrix object (mat)
+## return a list of below functions
+## getMatrix and setMatrix to an input Matrix object
+## getInverseMatrix and setInverseMatrix of input Matrix Object using Solve function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+  
+  invMat <- NULL
+  setMatrix <- function(y) {
+    mat <<- y
+    invMat <<- NULL
+  }
+  getMatrix <- function() mat
+  setInverseMatrix <- function(solve) invMat <<- solve
+  getInverseMatrix <- function() invMat
+  list(getMatrix =getMatrix, setMatrix = setMatrix,
+       setInverseMatrix = setInverseMatrix,
+       getInverseMatrix = getInverseMatrix)
+  
 }
 
+## Accepts a special object retuned by makeCacheMatrix function
+## Return a matrix that is the inverse of a matrix object,input to makeCacheMatrix function
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matObj, ...) {
+  
+  invMat<- matObj$getInverseMatrix()
+  if(!is.null(invMat)) {
+    ## getting inverse of the matrix using Cache
+    return(invMat)
+  }
+  ## computing inverse of the input matrix for the first time
+  data <- matObj$getMatrix()
+  invMat <- solve(data, ...)
+  matObj$setInverseMatrix(invMat)
+  invMat
 }
